0f3f53b608c95dbabbb546cafc8217deb11689d1
Dev <Dev.Sterling@outlook.com>
2026-04-12 23:47:04 +0530

    learned about datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Variable name can't be bigger than 255 characters
+    char a = 'A';
+    bool t = true;
+    string str = "star";
+    int x = 9;
+    float y = 10.001;
+    double z = 3.9982;
+    cout << x+a << '\n';
+    cout << a+t << '\n';
+    cout << str+x;
+    return 0;
+}
