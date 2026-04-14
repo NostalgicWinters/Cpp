ea1656a3c8a8eacd0dfa74bc7fa471ac6de16e62
Dev <Dev.Sterling@outlook.com>
2026-04-14 13:15:15 +0530

    removeDuplicates 2 solved

--- DSA/150leetcode.cpp
+++ DSA/150leetcode.cpp
@@ -25,3 +25,18 @@ int removeDuplicates(vector<int>& nums) {
         }
         return k;
     }
+
+int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0 || n==1|| n==2) return n;
+        map<int,int> hash;
+        int p = 0;
+        int k = 2;
+        for (int i = 2; i < n; i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
