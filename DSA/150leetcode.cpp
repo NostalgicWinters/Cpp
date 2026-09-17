763102a3be3905bd21b1415b77d67da9dd6ccd74
NostalgicWinters <Dev.Sterling@outlook.com>
2026-09-17 19:45:40 +0530

    Leetcode- Two Sum 2 input array is sorted

--- DSA/150leetcode.cpp
+++ DSA/150leetcode.cpp
@@ -110,4 +110,21 @@ bool canJump(vector<int>& nums) {
 
         return false;
     }
-};
+
+vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size()-1;
+        while(l<r){
+            int num = target-numbers[l];
+            if(numbers[r]==num){
+                return {l+1, r+1};
+            }
+            else if(numbers[r]>num){
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+        return {};
+
+    }
